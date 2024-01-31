120cee3d296a1a25944e23b53b438dc8fe2e3613
opilane <opilane@HP-CZC2349HTQ>
2024-01-31 16:13:58 +0200

    Работа в парах (Вариант 4)

--- PythonApplication2.py
+++ PythonApplication2.py
@@ -54,3 +54,67 @@
 #    print()
 #    print("Гипотеза верна")
 
+##V-4
+##Задание 1
+#def print_animals(n):
+#    for i in range(n):
+#        print("  ^---^")
+#        print(" ( o o )")
+#        print("  ! = !/)")
+#        if i < n - 1:  # Не добавляем пустой столбец после последнего зверька
+#            print()  # Пустая строка для разделения зверьков
+
+## Пример использования
+#n = int(input("Введите число зверьков (от 1 до 9): "))
+#if 1 <= n <= 9:
+#    print_animals(n)
+#else:
+#    print("Некорректный ввод. Введите число от 1 до 9.")
+
+##Задание 2
+#def print_powers(k,n):
+#    result=1
+#    while k**result<=n*100:
+#        print(f"{k}^{result}={k**result}")
+#        result+=1
+#k = int(input("Введите показатель степени k:"))
+#n = int(input("Введите число n:"))
+#print_powers(k,n)
+
+##Задание 3
+#import random
+
+#num_students = random.randint(20, 30)  
+#grades = [random.randint(1, 10) for _ in range(num_students)]
+#min_grade, max_grade = min(grades), max(grades)
+
+#print(f"Количество учеников в классе: {num_students}")
+#print(f"Оценки учеников: {grades}")
+#print(f"Минимальная оценка: {min_grade}")
+#print(f"Максимальная оценка: {max_grade}")
+
+
+
+##Задание 4
+#def calculate_amoeba_population(hours):
+#    return 2**(hours//3)
+#for time in range(3,25,3):
+#    amoeba_count=calculate_amoeba_population(time)
+#    print(f"Через{time}часов будет{amoeba_count}клеток.")
+
+#Задание 5
+print("vaariant for")
+from random import *
+K=randint(1,100)
+M=randint(1,10)
+s=0
+p=0
+print("у губки боба всего",K, "котлет(ы)")
+print("на одну сковородку помещается",M, "котлет(ы)")
+for b in range(0,K):
+    s+=1
+    if(s==M):
+        p+=1
+        s=0
+print("надо пожарить",p, "полных сковородок")
+print("на последней сковородке останится дожарить",p, "котлет(ы)")
